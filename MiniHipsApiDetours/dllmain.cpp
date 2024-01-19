3628d7f710d7bf0c8d50d11cfa62746f41c0e3a5
lyc8503 <me@lyc8503.site>
2024-01-19 23:18:41 +0800

    add debuglog

--- MiniHipsApiDetours/dllmain.cpp
+++ MiniHipsApiDetours/dllmain.cpp
@@ -8,6 +8,27 @@
 #pragma comment(lib, "pathcch.lib")
 
 
+#include <stdio.h>
+#include <stdarg.h>
+
+#ifdef _DEBUG
+void DebugPrint(const wchar_t* format, ...);
+void DebugPrint(const wchar_t* format, ...) {
+    wchar_t buffer[1024];
+    va_list args;
+
+    va_start(args, format);
+    _vsnwprintf_s(buffer, ARRAYSIZE(buffer) - 1, _TRUNCATE, format, args);
+    buffer[ARRAYSIZE(buffer) - 1] = 0; // Ensure null-termination
+    va_end(args);
+
+    OutputDebugStringW(buffer);
+}
+#else
+#define DebugPrint(format, ...) ((void)0)
+#endif
+
+
 static HANDLE(WINAPI* TrueCreateFileW)(LPCWSTR, DWORD, DWORD, LPSECURITY_ATTRIBUTES, DWORD, DWORD, HANDLE) = CreateFileW;
 
 
@@ -15,13 +36,13 @@ HANDLE WINAPI HookedCreateFileW(LPCWSTR lpFileName, DWORD dwDesiredAccess, DWORD
     DWORD dwCreationDisposition, DWORD dwFlagsAndAttributes, HANDLE hTemplateFile)
 {
     //MessageBox(NULL, L"Hooked CreateFileW", L"Hi", MB_OK);
+    DebugPrint(L"Hooked CreateFileW: %s", lpFileName);
 
     WCHAR canonicalPath[MAX_PATH + 100];
     HRESULT hr = PathCchCanonicalizeEx(canonicalPath, ARRAYSIZE(canonicalPath), lpFileName, PATHCCH_ALLOW_LONG_PATHS);
 
     if (!SUCCEEDED(hr)) {
         SetLastError(ERROR_INTERNAL_ERROR);
-        SetLastError(ERROR_OUT_OF_PAPER);
         return INVALID_HANDLE_VALUE;
     }
 
@@ -39,16 +60,19 @@ BOOL APIENTRY DllMain( HMODULE hModule,
                      )
 {
 
+    DebugPrint(L"Enter MiniHipsApiDetours.dll DllMain, PID: %lu", GetCurrentProcessId());
+
     // https://github.com/microsoft/Detours/wiki/OverviewHelpers
     // Immediately return TRUE if DetourIsHelperProcess return TRUE. 
     if (DetourIsHelperProcess()) {
+        DebugPrint(L"DetourIsHelperProcess() = true, return");
         return TRUE;
     }
 
     switch (ul_reason_for_call)
     {
     case DLL_PROCESS_ATTACH:
-        MessageBox(NULL, L"Hello from DLL", L"Hi", MB_OK);
+        // MessageBox(NULL, L"Hello from DLL", L"Hi", MB_OK);
         DetourTransactionBegin();
         DetourUpdateThread(GetCurrentThread());
         DetourAttach(&(PVOID&)TrueCreateFileW, HookedCreateFileW);
