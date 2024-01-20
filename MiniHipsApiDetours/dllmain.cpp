fc9d7d24b782c99744e9895965d96eef5130dd91
lyc8503 <me@lyc8503.site>
2024-01-20 19:42:48 +0800

    try hook process creation

--- MiniHipsApiDetours/dllmain.cpp
+++ MiniHipsApiDetours/dllmain.cpp
@@ -6,7 +6,7 @@
 #pragma comment(lib, "shlwapi.lib")
 #include <pathcch.h>
 #pragma comment(lib, "pathcch.lib")
-
+#include <shellapi.h>
 
 #include <stdio.h>
 #include <stdarg.h>
@@ -31,6 +31,12 @@ void DebugPrint(const wchar_t* format, ...) {
 
 static HANDLE(WINAPI* TrueCreateFileW)(LPCWSTR, DWORD, DWORD, LPSECURITY_ATTRIBUTES, DWORD, DWORD, HANDLE) = CreateFileW;
 
+static BOOL(WINAPI* TrueCreateProcessW)(LPCWSTR, LPWSTR, LPSECURITY_ATTRIBUTES, LPSECURITY_ATTRIBUTES, BOOL, DWORD, LPVOID, LPCWSTR, LPSTARTUPINFOW, LPPROCESS_INFORMATION) = CreateProcessW;
+static BOOL(WINAPI* TrueCreateProcessAsUserW)(HANDLE, LPCWSTR, LPWSTR, LPSECURITY_ATTRIBUTES, LPSECURITY_ATTRIBUTES, BOOL, DWORD, LPVOID, LPCWSTR, LPSTARTUPINFOW, LPPROCESS_INFORMATION) = CreateProcessAsUserW;
+
+static HINSTANCE(WINAPI* TrueShellExecuteW)(HWND, LPCWSTR, LPCWSTR, LPCWSTR, LPCWSTR, INT) = ShellExecuteW;
+
+
 
 HANDLE WINAPI HookedCreateFileW(LPCWSTR lpFileName, DWORD dwDesiredAccess, DWORD dwShareMode, LPSECURITY_ATTRIBUTES lpSecurityAttributes,
     DWORD dwCreationDisposition, DWORD dwFlagsAndAttributes, HANDLE hTemplateFile)
@@ -45,8 +51,10 @@ HANDLE WINAPI HookedCreateFileW(LPCWSTR lpFileName, DWORD dwDesiredAccess, DWORD
         SetLastError(ERROR_INTERNAL_ERROR);
         return INVALID_HANDLE_VALUE;
     }
-
+    
+    DebugPrint(L"canonicalPath: %s", canonicalPath);
     if (PathMatchSpecW(canonicalPath, TEXT("C:\\*.txt"))) {
+        DebugPrint(L"Rejected: %s", canonicalPath);
         SetLastError(ERROR_ACCESS_DENIED);
         return INVALID_HANDLE_VALUE;
     }
@@ -54,13 +62,47 @@ HANDLE WINAPI HookedCreateFileW(LPCWSTR lpFileName, DWORD dwDesiredAccess, DWORD
     return TrueCreateFileW(lpFileName, dwDesiredAccess, dwShareMode, lpSecurityAttributes, dwCreationDisposition, dwFlagsAndAttributes, hTemplateFile);
 }
 
+
+BOOL WINAPI HookedCreateProcessW(LPCWSTR lpApplicationName, LPWSTR lpCommandLine, LPSECURITY_ATTRIBUTES lpProcessAttributes,
+    LPSECURITY_ATTRIBUTES lpThreadAttributes, BOOL bInheritHandles, DWORD dwCreationFlags, LPVOID lpEnvironment,
+    LPCWSTR lpCurrentDirectory, LPSTARTUPINFOW lpStartupInfo, LPPROCESS_INFORMATION lpProcessInformation)
+{
+	MessageBox(NULL, L"Hooked CreateProcessW", L"Hi", MB_OK);
+	DebugPrint(L"Hooked CreateProcessW: %s", lpCommandLine);
+
+	return TrueCreateProcessW(lpApplicationName, lpCommandLine, lpProcessAttributes, lpThreadAttributes, bInheritHandles, dwCreationFlags,
+        		lpEnvironment, lpCurrentDirectory, lpStartupInfo, lpProcessInformation);
+}
+
+
+BOOL WINAPI HookedCreateProcessAsUserW(HANDLE hToken, LPCWSTR lpApplicationName, LPWSTR lpCommandLine, LPSECURITY_ATTRIBUTES lpProcessAttributes,
+    LPSECURITY_ATTRIBUTES lpThreadAttributes, BOOL bInheritHandles, DWORD dwCreationFlags, LPVOID lpEnvironment,
+    LPCWSTR lpCurrentDirectory, LPSTARTUPINFOW lpStartupInfo, LPPROCESS_INFORMATION lpProcessInformation)
+{
+	MessageBox(NULL, L"Hooked CreateProcessAsUserW", L"Hi", MB_OK);
+	DebugPrint(L"Hooked CreateProcessAsUserW: %s", lpCommandLine);
+
+	return TrueCreateProcessAsUserW(hToken, lpApplicationName, lpCommandLine, lpProcessAttributes, lpThreadAttributes, bInheritHandles, dwCreationFlags,
+        				lpEnvironment, lpCurrentDirectory, lpStartupInfo, lpProcessInformation);
+}
+
+
+HINSTANCE WINAPI HookedShellExecuteW(HWND hwnd, LPCWSTR lpOperation, LPCWSTR lpFile, LPCWSTR lpParameters, LPCWSTR lpDirectory, INT nShowCmd)
+{
+	MessageBox(NULL, L"Hooked ShellExecuteW", L"Hi", MB_OK);
+	DebugPrint(L"Hooked ShellExecuteW: %s", lpFile);
+
+	return TrueShellExecuteW(hwnd, lpOperation, lpFile, lpParameters, lpDirectory, nShowCmd);
+}
+
+
 BOOL APIENTRY DllMain( HMODULE hModule,
                        DWORD  ul_reason_for_call,
                        LPVOID lpReserved
                      )
 {
 
-    DebugPrint(L"Enter MiniHipsApiDetours.dll DllMain, PID: %lu", GetCurrentProcessId());
+    DebugPrint(L"Enter MiniHipsApiDetours.dll DllMain, PID: %lu, reason: %lu", GetCurrentProcessId(), ul_reason_for_call);
 
     // https://github.com/microsoft/Detours/wiki/OverviewHelpers
     // Immediately return TRUE if DetourIsHelperProcess return TRUE. 
@@ -76,6 +118,11 @@ BOOL APIENTRY DllMain( HMODULE hModule,
         DetourTransactionBegin();
         DetourUpdateThread(GetCurrentThread());
         DetourAttach(&(PVOID&)TrueCreateFileW, HookedCreateFileW);
+
+        DetourAttach(&(PVOID&)TrueCreateProcessW, HookedCreateProcessW);
+        DetourAttach(&(PVOID&)TrueCreateProcessAsUserW, HookedCreateProcessAsUserW);
+        
+        DetourAttach(&(PVOID&)TrueShellExecuteW, HookedShellExecuteW);
         DetourTransactionCommit();
         break;
     case DLL_THREAD_ATTACH:
--- MiniHipsPy/main.py
+++ MiniHipsPy/main.py
@@ -1,6 +1,10 @@
 import ctypes
 from ctypes import *
 
+import psutil
+
+all_processes = psutil.process_iter()
+
 helper = ctypes.CDLL("../x64/Debug/MiniHipsHelper.dll")
 print(helper)
 
@@ -8,5 +12,13 @@ InjectDll = helper.InjectDllPid
 InjectDll.argtypes = [c_ulong, c_char_p]
 InjectDll.restype = c_int
 
-result = InjectDll(14652, b"C:\\Users\\lyc\\Desktop\\MiniHIPS\\x64\\Debug\\MiniHipsApiDetours.dll")
-print("Result: ", result)
+
+for process in all_processes:
+    process_info = process.as_dict(attrs=['pid', 'name', 'username'])
+
+    if process_info['name'] != 'explorer.exe' and process_info['name'] != 'Notepad.exe':
+        continue
+    print(process_info)
+    result = InjectDll(process_info['pid'], b"C:\\Users\\lyc\\Desktop\\MiniHIPS\\x64\\Debug\\MiniHipsApiDetours.dll")
+    print(result)
+
